2a852baf424e8cc0448ca1ec614f61f32064a0ac
willian-silva7 <willian.1994@hotmail.com>
2021-05-17 01:19:31 +0100

    app

---
+++ precache-manifest.2d040dc5d4a990a9414e58c31ec87c79.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6892a65364dedb95ea7d82d9f5520ae9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f60e0c2bafce07a38455",
+    "url": "/static/css/main.c3c4ca21.chunk.css"
+  },
+  {
+    "revision": "ff559e96b2bf87a70f70",
+    "url": "/static/js/2.32b64221.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.32b64221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60e0c2bafce07a38455",
+    "url": "/static/js/main.dc5c6b68.chunk.js"
+  },
+  {
+    "revision": "c7bc135d738ad45d9e19",
+    "url": "/static/js/runtime-main.86361334.js"
+  },
+  {
+    "revision": "820536f2e5990820ef9274011bcd0c67",
+    "url": "/static/media/logo.820536f2.svg"
+  },
+  {
+    "revision": "cf078c75bd1304897e237ea1ace91307",
+    "url": "/static/media/signin-background.cf078c75.svg"
+  },
+  {
+    "revision": "48007e2096c2a11c9f3a882111f33c20",
+    "url": "/static/media/signup-background.48007e20.svg"
+  },
+  {
+    "revision": "21ce9bbfb385a5298794b37222b6e86f",
+    "url": "/static/media/user.21ce9bbf.jpg"
+  }
+]);
